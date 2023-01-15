d087c520538176ef28064bef2859863b4e918b0d
kubisense <87003603+kubisense@users.noreply.github.com>
2023-01-15 16:04:59 +0000

    initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,30 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3,5);
+// console.log(sum);
 
 // Arrow Function With Parameters
+/* const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+let sum = addTwoNumbers(3,5);
+console.log(sum); */
 
 // Single Line Arrow Function With Parameters
-
+const addTwoNumbers = (a, b) =>  a + b;
+let sum = addTwoNumbers(3,5);
+console.log(sum);
 // Implicit Returns
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+        This is a multiple string!
+        check it out on the output terminal!
+    </p>`
+)
+console.log(returnMultipleLines());
